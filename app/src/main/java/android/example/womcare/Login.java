8675166603fd20d5214b43c28cc4c93565e0a337
arunima8jan <arunima8jan@gmail.com>
2019-12-01 18:07:16 +0530

    first

--- app/src/main/java/android/example/womcare/Login.java
+++ app/src/main/java/android/example/womcare/Login.java
@@ -2,13 +2,26 @@ package android.example.womcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        button=(Button)findViewById(R.id.login);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Login.this,test.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
--- app/src/main/java/android/example/womcare/MainActivity.java
+++ app/src/main/java/android/example/womcare/MainActivity.java
@@ -1,10 +1,14 @@
 package android.example.womcare;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Button;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private static int SPLASH_TIME_OUT=4000;
